5889b5ca967a3f73cad95c406c66a761ceb58dc6
Maciej Sz <maciej.sz@gmail.com>
2014-12-21 19:03:07 +0100

    initial structure

---
+++ lib/src/overrides.dart
@@ -0,0 +1,5 @@
+part of oxio.unittest;
+
+void skip_test(String spec, TestFunction body){
+  print("SKIP: ${spec}");
+}
---
+++ lib/unittest.dart
@@ -0,0 +1,7 @@
+library oxio.unittest;
+
+import 'package:unittest/unittest.dart';
+
+export 'package:unittest/unittest.dart';
+
+part 'src/overrides.dart';
